37d430d4a670d18e5260206d5b7faef2bd64dca1
Amit Maurya <139799731+amit-maurya0099@users.noreply.github.com>
2025-04-30 00:04:55 +0530

    new resume

--- src/components/banner/LeftBanner.jsx
+++ src/components/banner/LeftBanner.jsx
@@ -35,7 +35,7 @@ const LeftBanner = () => {
         
       </div>
        <div className="-my-10 ">
-       <a href="https://drive.google.com/file/d/1Z_8EX5UtzOmXrTxuHgFvHW0yYmiCALq0/view?usp=sharing
+       <a href="https://drive.google.com/file/d/1ac6-QNax7fbrEmbfDInr0ph4ZTKQBjAg/view?usp=sharing
        "><button className="text-lg border border-black text-black bg-cyan-500 px-4 py-2 rounded-xl " >
           Download CV
         </button></a> </div>
